2a2dcff10488ac93148bec01b66ae167be12826c
Marcus BELMONT <marcus.belmont@viacesi.fr>
2022-02-03 17:19:47 +0100

    smooth hover
    Sael plane de maniere smooth : les gachettes ralentissent sa chute correctement. il faut maintenant pouvoir se diriger vers le haut ou vers le bas à vitesse constante

--- My project/Assets/Character/FlyingCharacter.cs
+++ My project/Assets/Character/FlyingCharacter.cs
@@ -6,22 +6,43 @@ using UnityEngine;
 
 public class FlyingCharacter : MonoBehaviour
 {
-    Vector3 speed = Vector3.up*3;
+    Vector3 speed = Vector3.up*8;
     // Start is called before the first frame update
     void Start()
     {
         transform.rotation = Quaternion.Euler(90,0, 00);
     }
     bool TriggerPressedLastFrame;
+    [Range(0.001f,0.009f)]
+    public float smooth = 1f;
     float gravity = 9.81f;
+    float PreviousAvgTrigger;
     float timeStartFalling;
+    Vector3 PreviousSpeed;
     // Update is called once per frame
     void Update()
     {
-       // speed = speed * Input.GetAxis("Vertical");
+        // speed = speed * Input.GetAxis("Vertical");
 
-        transform.Translate(speed * Time.deltaTime);
-        if (Input.GetAxis("RightTrigger") <= 0.9999 || Input.GetAxis("LeftTrigger") <= 0.9999)
+        transform.Translate(speed * Time.deltaTime);       
+        float avgTriggers = 1-((Input.GetAxis("RightTrigger") + Input.GetAxis("LeftTrigger")) / 2);
+        if (avgTriggers > PreviousAvgTrigger)
+        {
+            avgTriggers = PreviousAvgTrigger + (avgTriggers - PreviousAvgTrigger)*4f*Time.deltaTime;
+        }
+        else
+        {
+            avgTriggers = PreviousAvgTrigger - ( PreviousAvgTrigger - avgTriggers) * 2f*Time.deltaTime;
+        }
+        PreviousAvgTrigger = avgTriggers;
+       // avgTriggers = avgTriggers * 0.9f + 0.05f;
+        Vector3 FallingSpeed = Vector3.forward * gravity * Time.deltaTime * avgTriggers;
+        transform.Translate(FallingSpeed);
+        PreviousSpeed = FallingSpeed;
+    }
+     
+        
+       /* if (Input.GetAxis("RightTrigger") <= 0.9999 || Input.GetAxis("LeftTrigger") <= 0.9999)
         {
             if (!TriggerPressedLastFrame)
             {
@@ -35,7 +56,7 @@ public class FlyingCharacter : MonoBehaviour
         else
         {
             TriggerPressedLastFrame = false;
-        }
+        }*/
 
-    }
+    
 }
