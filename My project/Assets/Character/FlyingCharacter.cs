5fc8cd14b4ed151359edda59e018b75d71138d3f
Marcus BELMONT <marcus.belmont@viacesi.fr>
2022-02-09 19:07:25 +0100

    character is tiling correctly
    la vitesse de chute doit maintenant être convertie en vitesse d'avancée l'lorsque le personnage se remet à l'horizontale. Un smooth en fonction de la vitesse générale sera appliqué

--- My project/Assets/Character/FlyingCharacter.cs
+++ My project/Assets/Character/FlyingCharacter.cs
@@ -8,76 +8,57 @@ using UnityEngine.UI;
 
 public class FlyingCharacter : MonoBehaviour
 {
-    Vector3 speed = Vector3.up*4;
+    Vector3 speed = Vector3.forward*4;
     // Start is called before the first frame update
     void Start()
     {
     
-        transform.rotation = Quaternion.Euler(90,0, 00);
+       
     }
+    public Text AngleDisplay;
     public Text avgTriggerDisplay;
     public Text FallingSpeedDisplay;
-    bool TriggerPressedLastFrame;
     [Range(0.001f,0.009f)]
     public float smooth = 1f;
     float gravity = 00.04f;
-    float PreviousAvgTrigger;
-    float timeStartFalling;
     float PreviousSpeed;
     // Update is called once per frame
     void Update()
     {
-        // speed = speed * Input.GetAxis("Vertical");
-     
-        transform.Translate(speed * Time.deltaTime);       
-        float avgTriggers = (Input.GetAxis("RightTrigger") + Input.GetAxis("LeftTrigger")) / 2f;
+        Quaternion rotationStick = Quaternion.Euler(Input.GetAxis("Vertical") * 90, 0, 0);
+        transform.rotation = rotationStick;
+        AngleDisplay.text = rotationStick.normalized.x.ToString();
+;        transform.Translate(speed * Time.deltaTime);       
+        float avgTriggers = (Input.GetAxis("RightTrigger") + Input.GetAxis("LeftTrigger") ) / 2f;
         avgTriggerDisplay.text = avgTriggers.ToString();
+        
         avgTriggers = 1f - avgTriggers;
-        /*
-        if (avgTriggers > PreviousAvgTrigger)
-            {
-                //avgTriggers = PreviousAvgTrigger + (avgTriggers - PreviousAvgTrigger) * 4f * Time.deltaTime;
-            }
-            else
-            {
-       
-                    avgTriggers = PreviousAvgTrigger - (PreviousAvgTrigger - avgTriggers) * 0.01f * Time.deltaTime;
-               
-            }
-            if (avgTriggers > 1)
-        {
-            avgTriggers = 1;
-        }*/
+        avgTriggers += rotationStick.normalized.x * (1f - avgTriggers);
+
+
         float speedValue = PreviousSpeed + (gravity * Time.deltaTime * avgTriggers);
-        PreviousAvgTrigger = avgTriggers;
         
-        
-        // avgTriggers = avgTriggers * 0.9f + 0.05f;
-        //Vector3 FallingSpeed = Vector3.forward * gravity * Time.deltaTime * avgTriggers + (PreviousSpeed* 0.9);
-        
-     
-        //
-        transform.Translate(Vector3.forward * speedValue);
+        transform.Translate(Vector3.down * speedValue, Space.World);
         FallingSpeedDisplay.text = speedValue.ToString();
         PreviousSpeed = speedValue;
     }
-     
-        
-       /* if (Input.GetAxis("RightTrigger") <= 0.9999 || Input.GetAxis("LeftTrigger") <= 0.9999)
-        {
-            if (!TriggerPressedLastFrame)
-            {
-                timeStartFalling = Time.time;
-                TriggerPressedLastFrame = true;
-            }
-            float FallingTime = Time.time - timeStartFalling;
-            float avgTriggers = 1-((Input.GetAxis("RightTrigger") + Input.GetAxis("LeftTrigger"))/2);
-            transform.Translate(Vector3.forward * gravity * Time.deltaTime * FallingTime * avgTriggers);
-        }
-        else
-        {
-            TriggerPressedLastFrame = false;
-        }*/
 
-    
+
+
+
+    /*
+       if (avgTriggers > PreviousAvgTrigger)
+           {
+               //avgTriggers = PreviousAvgTrigger + (avgTriggers - PreviousAvgTrigger) * 4f * Time.deltaTime;
+           }
+           else
+           {
+
+                   avgTriggers = PreviousAvgTrigger - (PreviousAvgTrigger - avgTriggers) * 0.01f * Time.deltaTime;
+
+           }
+           if (avgTriggers > 1)
+       {
+           avgTriggers = 1;
+       }*/
 }
