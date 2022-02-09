55933bf33ac8124b3f227d853967e77b34445e7e
Marcus BELMONT <marcus.belmont@viacesi.fr>
2022-02-09 16:07:25 +0100

    sael is hovering properly
    il plane mais il remonte pas. la prochaine étape consistera à orienter le modèle et ses axes par rapport à son déplacement et modifier la vitesse par rapport à la gravité

--- My project/Assets/Character/FlyingCharacter.cs
+++ My project/Assets/Character/FlyingCharacter.cs
@@ -2,43 +2,63 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FlyingCharacter : MonoBehaviour
 {
-    Vector3 speed = Vector3.up*8;
+    Vector3 speed = Vector3.up*4;
     // Start is called before the first frame update
     void Start()
     {
+    
         transform.rotation = Quaternion.Euler(90,0, 00);
     }
+    public Text avgTriggerDisplay;
+    public Text FallingSpeedDisplay;
     bool TriggerPressedLastFrame;
     [Range(0.001f,0.009f)]
     public float smooth = 1f;
-    float gravity = 9.81f;
+    float gravity = 00.04f;
     float PreviousAvgTrigger;
     float timeStartFalling;
-    Vector3 PreviousSpeed;
+    float PreviousSpeed;
     // Update is called once per frame
     void Update()
     {
         // speed = speed * Input.GetAxis("Vertical");
-
+     
         transform.Translate(speed * Time.deltaTime);       
-        float avgTriggers = 1-((Input.GetAxis("RightTrigger") + Input.GetAxis("LeftTrigger")) / 2);
+        float avgTriggers = 1f-(Input.GetAxis("RightTrigger") + Input.GetAxis("LeftTrigger")) / 2f;
+        avgTriggerDisplay.text = avgTriggers.ToString();
+        /*
         if (avgTriggers > PreviousAvgTrigger)
+            {
+                //avgTriggers = PreviousAvgTrigger + (avgTriggers - PreviousAvgTrigger) * 4f * Time.deltaTime;
+            }
+            else
+            {
+       
+                    avgTriggers = PreviousAvgTrigger - (PreviousAvgTrigger - avgTriggers) * 0.01f * Time.deltaTime;
+               
+            }
+            if (avgTriggers > 1)
         {
-            avgTriggers = PreviousAvgTrigger + (avgTriggers - PreviousAvgTrigger)*4f*Time.deltaTime;
-        }
-        else
-        {
-            avgTriggers = PreviousAvgTrigger - ( PreviousAvgTrigger - avgTriggers) * 2f*Time.deltaTime;
-        }
+            avgTriggers = 1;
+        }*/
+        float speedValue = PreviousSpeed + (gravity * Time.deltaTime * avgTriggers);
         PreviousAvgTrigger = avgTriggers;
-       // avgTriggers = avgTriggers * 0.9f + 0.05f;
-        Vector3 FallingSpeed = Vector3.forward * gravity * Time.deltaTime * avgTriggers;
-        transform.Translate(FallingSpeed);
-        PreviousSpeed = FallingSpeed;
+        
+        
+        // avgTriggers = avgTriggers * 0.9f + 0.05f;
+        //Vector3 FallingSpeed = Vector3.forward * gravity * Time.deltaTime * avgTriggers + (PreviousSpeed* 0.9);
+        
+     
+        //
+        transform.Translate(Vector3.forward * speedValue);
+        FallingSpeedDisplay.text = speedValue.ToString();
+        PreviousSpeed = speedValue;
     }
      
         
